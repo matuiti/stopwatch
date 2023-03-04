44c769c231c2fdc2227b25a754922ad1a8a8e7b6
matuiti <ichi.animo@gmail.com>
2023-03-04 17:12:16 +0900

    初期→一気に完成まで

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-function App() {
+import React from 'react';
+import Stopwatch from './Stopwatch';
+
+const App = () => {
   return (
-    <div className="App bg-green-400">
-      てすとです
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold mb-8">Stopwatch App</h1>
+      <Stopwatch />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/StartStopButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const StartStopButton = ({ isRunning, handleStartStop }) => {
+  return (
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={handleStartStop}
+    >
+      {isRunning ? 'Stop' : 'Start'}
+    </button>
+  );
+};
+
+export default StartStopButton;
